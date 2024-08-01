0d2b41aa019f59261ad1db6aab870ddc090c23c1
Sreejita2023 <sensreejita67@gmail.com>
2024-08-01 20:56:00 +0530

    tailwind added

---
+++ src/utils/Friends.js
@@ -0,0 +1,27 @@
+export const data=[
+    {
+        id:1,
+        name:'Shreyash ',
+        status:'online'
+    },
+    {
+        id:2,
+        name:'Susmita ',
+        status:'busy'
+    },
+    {
+        id:3,
+        name:'Rick ',
+        status:'busy'
+    },
+    {
+        id:4,
+        name:'Abhay ',
+        status:'offline'
+    },
+    {
+        id:5,
+        name:'Sandhya',
+        status:'offline'
+    }
+]

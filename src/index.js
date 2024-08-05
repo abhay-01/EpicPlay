10d8f3c54894f4ce007c29dea3e24e5c515d3aea
Sreejita2023 <sensreejita67@gmail.com>
2024-08-05 08:59:33 +0530

    Added Friend Page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './output.css'
+import './input.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

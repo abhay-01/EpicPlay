7fba6227e64f4b000e13b73240909af9e22247cd
abhay-01 <pratapsinghabhay999@gmail.com>
2024-08-06 16:35:24 +0530

    chess board styling

--- src/games/chess/app.js
+++ src/games/chess/app.js
@@ -41,6 +41,51 @@ io.on("connection", (socket) => {
     else{
         socket.emit("err", "Room is full");
     }
+
+    socket.on("disconnect", () => {
+        if(player.white === socket.id){
+            player.white = null;
+        }
+        if(player.black === socket.id){
+            player.black = null;
+        }
+    });
+
+    socket.on("move", (msg) => {
+       try{
+
+        //inn if conditions se check kr rhe h k kis player ka turn h
+        //agar white ki turn hai or black player ne move kiya to usko error msg bhej do
+        if(chess.turn()=== "w" && socket.id!== player.white){
+            socket.emit("err", "Its not your turn");
+            return;
+        }
+
+        if(chess.turn()=== "b" && socket.id!== player.black){
+            socket.emit("err", "Its not your turn");
+            return;
+        }
+
+        //agr move valid h to usko move kr do
+        const move = chess.move(msg);
+        if(move){
+            currentPlayer = chess.turn(); //agr move valid h to turn change kr do
+            io.emit("move", msg); //sabko move bhej do
+            io.emit("boardState", chess.fen()); //sabko board ki state bhej do. fen function se board ki state nikal rhe h
+        }else{
+            console.log("Invalid move", msg);
+            socket.emit("err", "Invalid move");
+        }
+       }
+       catch(err){
+                console.log("Invalid move", msg);
+              socket.emit("err", "Invalid move");
+              return;
+       }
+    });
+
+
+
 });
 
 
--- src/games/chess/public/js/chessgame.js
+++ src/games/chess/public/js/chessgame.js
@@ -1,2 +1,91 @@
+
 const socket = io(); 
 
+
+const chess = new Chess();
+const boardElement = document.querySelector(".chessboard");
+
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {
+    const board = chess.board();
+
+    boardElement.innerHTML = "";
+    board.forEach((row, i) => {
+        row.forEach((square,j) => {
+            const squareElement = document.createElement("div");
+            squareElement.classList.add("square",
+            (i+j)%2 === 0 ? "light" : "dark");
+            squareElement.dataset.row = i;
+            squareElement.dataset.col = j;
+
+            if(square){
+                const pieceElement = document.createElement("div");
+                pieceElement.classList.add("piece",
+                    square.color === "w" ? "white" : "black",
+                );
+                pieceElement.innerText = getPieceUnicode(square);
+                pieceElement.draggable = playerRole === square.color;
+
+                pieceElement.addEventListener("dragstart",(e)=>{
+                    if(pieceElement.draggable){
+                        draggedPiece = pieceElement;
+                        sourceSquare = {row: i, col: j};
+                        e.dataTransfer.setData("text/plain", ""); //dragged piece ko move krne k liye
+                    }
+                });
+
+                pieceElement.addEventListener("dragend",()=>{
+                    draggedPiece = null;
+                    sourceSquare = null;
+                });
+
+                squareElement.appendChild(pieceElement);
+            }
+
+            squareElement.addEventListener("dragover", (e)=>{
+                e.preventDefault();
+            });
+
+            squareElement.addEventListener("drop",(e)=>{
+                e.preventDefault();
+
+                if(draggedPiece){
+                    const targetSource = {
+                        row: parseInt(squareElement.dataset.row),
+                        col: parseInt(squareElement.dataset.col),
+                    };
+
+                    handleMove(sourceSquare, targetSource);
+                }
+
+            });
+          boardElement.appendChild(squareElement);
+        });
+    });
+
+}
+
+const handleMove = () => {}
+
+const getPieceUnicode = (piece) => {
+    const unicodePieces = {
+        p: "\u2659",
+        r: "\u2656",
+        n: "\u2658",
+        b: "\u2657",
+        q: "\u2655",
+        k: "\u2654",
+        P: "\u265F",
+        R: "\u265C",
+        N: "\u265E",
+        B: "\u265D",
+        Q: "\u265B",
+        K: "\u265A",
+    }
+
+    return unicodePieces[piece.type] || "";
+}

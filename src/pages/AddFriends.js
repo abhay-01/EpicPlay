6ffa08688971ea7a3b9c4061f54feaad25e850a6
Sandhya Gupta <guptasandhya617@gmail.com>
2024-09-01 22:22:38 +0530

    added

--- src/pages/AddFriends.js
+++ src/pages/AddFriends.js
@@ -16,14 +16,17 @@ const AddFriends = () => {
       }}
     >
       {/* Search Bar */}
-      <div style={{ padding: "20px", textAlign: "center" }} className="border-b border-white border-opacity-35 ">
+      <div
+        style={{ padding: "20px", textAlign: "center" }}
+        className="border-b border-white border-opacity-35 "
+      >
         <input
           type="text"
           placeholder="Search for games..."
           className="w-10/12 py-2 border  rounded-md text-[16px] bg-transparent px-2"
         />
       </div>
-      
+
       {/* Header Image */}
       <div
         className={`flex flex-col items-start pl-[50px] `}
@@ -35,39 +38,43 @@ const AddFriends = () => {
           backgroundPosition: "center",
         }}
       >
-        
         <div className="font-semibold text-xl my-4 ">Add Friends</div>
-        <div className="w-11/12 flex flex-col items-center gap-y-4 border rounded-md py-12  " style={{
-          width: "100%",
-          backgroundImage: `url(${bg})`,
-          backgroundRepeat: "repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}>
-        <input
-          type="text"
-          placeholder="Search for games..."
-          className="w-11/12 py-2 border border-white border-opacity-40   rounded-md text-[16px] bg-transparent px-2"
-        />
-        <div className="font-bold text-xl text-white text-opacity-70">OR</div>
-        <input
-          type="text"
-          placeholder="Search for games..."
-          className="w-11/12 py-2 border border-white border-opacity-40  rounded-md text-[16px] bg-transparent px-2"
-        />
+        <div
+          className="w-11/12 flex flex-col items-center gap-y-4 border rounded-md py-12  "
+          style={{
+            width: "100%",
+            backgroundImage: `url(${bg})`,
+            backgroundRepeat: "repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Enter friend username"
+            className="w-11/12 py-2 border border-white border-opacity-40   rounded-md text-[16px] bg-transparent px-2"
+          />
+          <div className="font-bold text-xl text-white text-opacity-70">OR</div>
+          <input
+            type="text"
+            placeholder="Enter friend code"
+            className="w-11/12 py-2 border border-white border-opacity-40  rounded-md text-[16px] bg-transparent px-2"
+          />
         </div>
         <div className="font-semibold text-xl my-4 ">Recommended</div>
-        <div className="grid grid-cols-3 w-full">
-        {data.map((item) => (
-          <div className="border flex flex-col rounded-xl items-center gap-y-4 my-4 py-4 px-8 w-10/12 transition duration-300 ease-in-out hover:scale-105 ">
-            <img src={icon} width="60px" height="60px" className="" />
-            <div className="text-lg font-bold ">{item.name}</div>
-            <div className="text-white text-opacity-40">Bio/Air</div>
-            <div className="bg-[#202320] text-white text-opacity-70 text-lg py-1 px-2 rounded-2xl">+Friend</div>
-          </div>
-        ))}
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full p-4">
+          {data.map((item) => (
+            <div className="border flex flex-col rounded-xl items-center gap-y-4 my-4 py-4 px-8 w-full sm:w-9/12 transition duration-300 ease-in-out hover:scale-105">
+              <img src={icon} width="60px" height="60px" className="" />
+              <div className="text-lg font-bold ">{item.name}</div>
+              <div className="text-white text-opacity-40">Bio/Air</div>
+              <div className="bg-[#202320] text-white text-opacity-70 text-lg py-1 px-2 rounded-2xl">
+                +Friend
+              </div>
+            </div>
+          ))}
         </div>
-        
       </div>
     </div>
   );
--- src/pages/Faqs.js
+++ src/pages/Faqs.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import bg from "../assets/bg.svg";
 import { FaGem, FaSearch, FaChevronDown } from "react-icons/fa";
+import boy from "../images/boy.png";
 
 const Faqs = () => {
   const [language, setLanguage] = useState("ENG");
@@ -77,7 +78,7 @@ const Faqs = () => {
         </div>
 
         <div
-          className={`flex flex-col   items-center justify-center flex-1`}
+          className={`flex flex-col justify-center items-center   pl-10 flex-1`}
           style={{
             width: "100%",
             backgroundImage: `url(${bg})`,
@@ -85,7 +86,46 @@ const Faqs = () => {
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
-        ></div>
+        >
+          <div className="flex flex-col p-4">
+            <h1 className="text-xl font-bold  text-white p-2 pl-8 mb-4">
+              Game Matching
+            </h1>
+            <div className="flex   p-2 pl-8 mb-4">
+              <div className="flex flex-col  border border-white w-[300px] h-[330px] m-2 m-r-2 rounded-lg p-10 pt-9 ">
+                <div className="flex justify-center items-center">
+                  <img src={boy} alt="Boy" className="w-[55px] h-[55px]" />
+                </div>
+                <div className="flex flex-col justify-center items-center p-5">
+                  <span className="text-xl">Sandhya Gupta</span>
+                  <span className="text-gray-500 pt-3">BIO/AIR</span>
+                </div>
+                <div className="flex items-center justify-center mr-4 pt-0">
+                  <div className="flex justify-center items-center bg-gray-800 rounded-full w-6 h-6 mr-1">
+                    <FaGem size={13} className="text-white" />
+                  </div>
+                  <div className="ml-0 flex flex-col text-white">
+                    <span className="text-xs font-light mb-0 pb-0">Coins</span>
+                    <span className="text-sm font-bold">00</span>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center">
+                  <button className="bg-orange-500 w-20 h-10 rounded-lg">
+                    STAKES
+                  </button>
+                </div>
+              </div>
+              <div className="flex items-center justify-center border border-white w-[300px] h-[330px] m-2 ml-6">
+                <img src={boy} alt="Boy" className="w-[25px] h-[25px]" />
+              </div>
+            </div>
+            <div className="flex  justify-center items-center">
+              <button className="bg-green-400 w-40 h-10 rounded-full">
+                Let's Play
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
